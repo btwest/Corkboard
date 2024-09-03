e0a541a2ba9523d23f1ccc805fbbcba1b2d87ada
Brendan West <brendantwest@gmail.com>
2024-09-03 07:03:20 -0400

    Setup Liveblocks

---
+++ components/confirm-modal.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+  disabled?: boolean;
+  header: string;
+  description?: string;
+}
+
+export const ConfirmModal = ({
+  children,
+  onConfirm,
+  disabled,
+  header,
+  description,
+}: ConfirmModalProps) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{header}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={disabled} onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
